8bfb5addcca1485130e7d8bf80c29b0f67e304c9
apterek <danialsud@gmail.com>
2022-12-26 21:49:17 +0300

    update

--- app/api/tables/views.py
+++ app/api/tables/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import viewsets
-
+from rest_framework.decorators import api_view
 from api.tables.serializers import UserSerializer, PersonalSerializer, CameraSerializer, CabinetSerializer, TimeStampSerializer
 from core.models import Personal, Cameras, Cabinets, DateTimeStamp
 
@@ -8,27 +8,32 @@ from core.models import Personal, Cameras, Cabinets, DateTimeStamp
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    http_method_names = ['get']
 
 
 class PersonalViewSet(viewsets.ModelViewSet):
     queryset = Personal.objects.all()
     serializer_class = PersonalSerializer
     permission_classes = []
+    http_method_names = ['get']
 
 
 class CamerasCabinetsView(viewsets.ModelViewSet):
     queryset = Cameras.objects.all()
     serializer_class = CameraSerializer
     permission_classes = []
+    http_method_names = ['get']
 
 
 class CabinetsView(viewsets.ModelViewSet):
     queryset = Cabinets.objects.all()
     serializer_class = CabinetSerializer
     permission_classes = []
+    http_method_names = ['get']
 
 
 class TimeStampView(viewsets.ModelViewSet):
     queryset = DateTimeStamp.objects.order_by("timedate")[:10]
     serializer_class = TimeStampSerializer
     permission_classes = []
+    http_method_names = ['get']
--- app/api/urls.py
+++ app/api/urls.py
@@ -1,10 +1,7 @@
 from django.urls import include, path
-from rest_framework import routers, views
+from rest_framework import routers
 from api.tables.views import UserViewSet, PersonalViewSet, CamerasCabinetsView, CabinetsView, TimeStampView
-"""from api.products.views import ProductsViewSet
 from api.users.views import RegisterViewSet, LoginView, LogoutView
-from api.purchase.views import PurchaseViewSet, PurchaseUserViewSet, AddPurchaseViewSet"""
-
 
 app_name = "api"
 
@@ -13,15 +10,11 @@ router.register(r"personal", PersonalViewSet, "personal")
 router.register(r"users", UserViewSet, "users")
 router.register(r"cameras", CamerasCabinetsView, "cameras")
 router.register(r"cabinets", CabinetsView, "cabinets")
-
 router.register(r"time", TimeStampView, "time")
-"""router.register(r"login", LoginView, "login")
+
+router.register(r"register", RegisterViewSet, "register")
+router.register(r"login", LoginView, "login")
 router.register(r"logout", LogoutView, "logout")
-
-router.register(r"purchase", PurchaseViewSet, "purchases")
-router.register(r"user", PurchaseUserViewSet, "user")
-router.register(r"add_purchase", AddPurchaseViewSet, "add_purchase")"""
-
 
 urlpatterns = [
     path("", include(router.urls)),
